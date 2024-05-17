8b8a5937e4c541ad4da09d6dd99c24b72f7fc3a5
Juan Londoño <juandavid1237890@gmail.com>
2024-05-17 17:52:59 -0500

    Ajuste Rutas

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,18 @@ import Menus from '@/views/Menus.vue'
 import Mesas from '@/views/Mesas.vue'
 import Reservas from '@/views/Reservas.vue'
 import Platos from '@/views/Platos.vue'
+import EditarCliente from '../components/Cliente/EditarCliente.vue'
+import NewCliente from '../components/Cliente/NewCliente.vue'
+import EditarMenu from '../components/Menu/EditarMenu.vue'
+import NewMenu from '../components/Menu/NewMenu.vue'
+import EditarMesa from '../components/Mesa/EditarCliente.vue'
+import NewMesa from '../components/Mesa/NewMesa.vue'
+import EditarReserva from '../components/Reserva/EditarReserva.vue'
+import NewReserva from '../components/Reserva/NewReserva.vue'
+import EditarPlato from '../components/Plato/EditarPlato.vue'
+import NewPlato from '../components/Plato/NewPlato.vue'
+
+
 
 const routes = [
   {
@@ -44,6 +56,82 @@ const routes = [
     name: 'Platos',
     component: Platos
   },
+
+
+
+  {
+    path: '/editar-cliente/:id',
+    name: 'EditarCliente',
+    component:  EditarCliente 
+  },
+
+  {
+    path: '/add-cliente/',
+    name: 'NewCliente',
+    component:  NewCliente 
+  },
+
+
+
+  {
+    path: '/editar-menu/:id',
+    name: 'EditarMenu',
+    component:  EditarMenu 
+  },
+
+  {
+    path: '/add-menu/',
+    name: 'NewMenu',
+    component:  NewMenu 
+  },
+
+
+
+
+  {
+    path: '/editar-mesa/:id',
+    name: 'EditarMesas',
+    component:  EditarMesa 
+  },
+
+  {
+    path: '/add-mesa/',
+    name: 'NewMesa',
+    component:  NewMesa 
+  },
+
+
+
+  {
+    path: '/editar-reserva/:id',
+    name: 'EditarReserva',
+    component:  EditarReserva 
+  },
+
+  {
+    path: '/add-reserva/',
+    name: 'NewReserva',
+    component:  NewReserva 
+  },
+
+
+  {
+    path: '/editar-plato/:id',
+    name: 'EditarPlato',
+    component:  EditarPlato 
+  },
+
+  {
+    path: '/add-plato/',
+    name: 'NewPlato',
+    component:  NewPlato 
+  },
+
+
+  
+
+
+
 
 
 
